001897f2148d9de5490b8bd1383e1ef2a10f3f48
Alexander Bott <Abott1222@gmail.com>
2018-02-21 12:52:45 -0800

    through lesson 2

---
+++ S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/WeatherList.java
@@ -0,0 +1,24 @@
+package com.example.android.sunshine;
+
+/**
+ * Created by alex on 2/21/18.
+ */
+
+
+public final class WeatherList {
+
+    /**
+     * This method returns a list of 100 popular toy names from the 20th and early 21st centuries.
+     * I don't know about you guys, but this definitely brings me back to my childhood...
+     *
+     * @return A list of popular toys
+     */
+    public static String[] getWeatherData() {
+        return new String[] {
+                "72",
+                "35",
+                "26",
+                "100"
+            };
+        }
+    }
